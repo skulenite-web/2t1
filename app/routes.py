ff6cc3f7bd4701d83a68c67dcc9a26b37318e294
Gabe Sher <gabriel.henry.sher@gmail.com>
2021-03-16 01:06:19 -0400

    A huge number of changes because I keep forgetting this is a git repo.
    * Created announcement landing page and obfuscated rest of site for now
    * Improved Meet the Team page, implemented Goodies
    * Many QoL / aesthetic improvements
    * Implemented scroll-by loading for team images
    * Modified nav/hero appearance
    * Further modularized templateswq

--- app/routes.py
+++ app/routes.py
@@ -11,35 +11,37 @@ from app import db
 
 # Landing - Intro & Main Page
 @app.route('/')
-@app.route('/index')
-@app.route('/home')
+def comingsoon():
+    return render_template('comingsoon.html')
+
+@app.route('/shallnotpass/index')
+@app.route('/shallnotpass/home')
 def home():
     visited = request.cookies.get('visited')
     if (visited == 'true'):
         # Have visited - skip animation
-#        db.people.insert({'name' : 'allison', 'role':'crew'})
         return render_template('watch.html')
     else:
-        # New visiton - play animation
+        # New visitor - play animation
         resp = make_response(render_template('intro_animation.html'))
         resp.set_cookie('visited', 'true')
         return resp
 
 ## Subpage - force animation
-@app.route('/intro')
+@app.route('/shallnotpass/intro')
 def intro():
     return render_template('intro_animation.html')
 
 ## Subpage: - Direct to watch
-@app.route('/watch')
+@app.route('/shallnotpass/watch')
 def watch():
     resp = make_response(redirect(url_for('home', _anchor='video')))
     resp.set_cookie('visited', 'true')
     return resp
 
 # Team 
-@app.route('/team')
-@app.route('/meettheteam')
+@app.route('/shallnotpass/team')
+@app.route('/shallnotpass/meettheteam')
 def team():
     cast = []
     crew = []
@@ -57,56 +59,59 @@ def team():
             creative.append(person)
     return render_template('team.html', cast=cast, crew=crew, band=band, creative=creative)
 
-@app.route('/cast')
+@app.route('/shallnotpass/cast')
 def cast():
     return redirect(url_for('team', _anchor='cast'))
-@app.route('/crew')
+@app.route('/shallnotpass/crew')
 def crew():
     return redirect(url_for('team', _anchor='crew'))
-@app.route('/band')
+@app.route('/shallnotpass/band')
 def band():
     return redirect(url_for('team', _anchor='band'))
-@app.route('/creative')
+@app.route('/shallnotpass/creative')
 def creative():
     return redirect(url_for('team', _anchor='creative'))
 
 # History
-@app.route('/history')
+@app.route('/shallnotpass/history')
 def history():
     return """history"""
 
 # Goodies
-@app.route('/goodies')
+@app.route('/shallnotpass/goodies')
 def goodies():
-    return """sketches and lyrics coming soon"""
+    sketch = [{'character':'Alice', 'line':'Hey Bob!'}, {'character':'Bob', 'line':'Alice, what the fuck are you doing here!?'}]
+    
+    return render_template('goodies.html', sketch=sketch)
 
-@app.route('/sketches')
+@app.route('/shallnotpass/sketches')
 def sketches():
     return redirect(url_for('goodies', _anchor='sketches'))
-@app.route('/lyrics')
+@app.route('/shallnotpass/lyrics')
 def lyrics():
     return redirect(url_for('goodies', _anchor='lyrics'))
-@app.route('/promo')
+@app.route('/shallnotpass/promo')
 def promo():
     return redirect(url_for('goodies', _anchor='promo'))
 
 # Behind the scenes
-@app.route('/bts')
-@app.route('/BTS')
-@app.route('/behindthescenes')
+@app.route('/shallnotpass/bts')
+@app.route('/shallnotpass/BTS')
+@app.route('/shallnotpass/behindthescenes')
 def bts():
     return """behind the scenes content here"""
 
-# 1T00
-@app.route('/1t00')
-@app.route('/1T00')
-def oneteehundred():
+# Donor Thanks
+@app.route('/shallnotpass/thanks')
+@app.route('/shallnotpass/1t00')
+@app.route('/shallnotpass/1T00')
+def thanks():
     return """1t00 babyyyyy!!!"""
 
 # Contact
-@app.route('/contact')
-@app.route('/social')
-@app.route('/socials')
+@app.route('/shallnotpass/contact')
+@app.route('/shallnotpass/social')
+@app.route('/shallnotpass/socials')
 def contact():
     return """socials and contact info"""
 
