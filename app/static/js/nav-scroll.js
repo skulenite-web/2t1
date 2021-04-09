ba4a3e7316349edc4ad38faa8dfd0fc78f73edad
Gabe Sher <gabriel.henry.sher@gmail.com>
2021-04-09 16:28:41 -0400

    Changed height for nav logo appearance

--- app/static/js/nav-scroll.js
+++ app/static/js/nav-scroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {navScroll()};
 
 function navScroll() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById("navlogo_img").style.width = '80px';
         document.getElementById("navlogo_img").style.opacity= '1';
     } else {
